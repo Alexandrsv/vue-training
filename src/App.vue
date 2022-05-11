d587a738a0314cdadce3db72f49e13b8c7e27b1d
Александр <zbc@mail.ru>
2022-05-11 15:38:05 +0300

    add simple form amd posts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <h1>Vue 3</h1>
-    <button @click="addLike">Like</button>
-    <button @click="addDislike">Dislike</button>
-    <div>
-      Количество лайков: <strong>{{ likes }}</strong>
-    </div>
-    <div>
-      Количество дизлайков: <strong>{{ dislikes }}</strong>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        type="text"
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        placeholder="Название поста"
+      />
+      <input
+        type="text"
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        placeholder="Содержание поста"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Содержание: </strong>{{ post.body }}</div>
     </div>
   </div>
 </template>
@@ -16,19 +29,81 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {
+          id: 1,
+          title: "Пост о JS",
+          body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, commodi deserunt ea, fugit ipsa, laudantium maiores molestias perspiciatis porro quibusdam quo sequi voluptatum! Earum, id, soluta! A, eaque, quidem.",
+        },
+        {
+          id: 2,
+          title: "Пост о " + Math.floor(Math.random() * 100),
+          body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, commodi deserunt ea, fugit ipsa, laudantium maiores molestias perspiciatis porro quibusdam quo sequi voluptatum! Earum, id, soluta! A, eaque, quidem.",
+        },
+        {
+          id: 3,
+          title: "Пост о " + Math.floor(Math.random() * 100),
+          body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at, commodi deserunt ea, fugit ipsa, laudantium maiores molestias perspiciatis porro quibusdam quo sequi voluptatum! Earum, id, soluta! A, eaque, quidem.",
+        },
+      ],
+      title: "",
+      body: "",
     };
   },
   methods: {
-    addLike() {
-      this.likes++;
-    },
-    addDislike() {
-      this.dislikes++;
+    createPost() {
+      this.posts.push({
+        id: this.posts.length + 1,
+        title: this.title,
+        body: this.body,
+      });
+
+      this.title = "";
+      this.body = "";
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+}
+
+.btn {
+  margin-bottom: 20px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: teal;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  justify-self: flex-start;
+}
+
+.input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 10px;
+  outline: none;
+}
+
+.post {
+  padding: 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+}
+</style>
