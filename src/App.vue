36277ed7fe499e064f5ca10a2a0263dcde2c46f4
Александр <zbc@mail.ru>
2022-05-11 15:07:18 +0300

    add simple base component

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Vue 3</h1>
+    <button @click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    <div>
+      Количество лайков: <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Количество дизлайков: <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes++;
+    },
+    addDislike() {
+      this.dislikes++;
+    },
+  },
+};
+</script>
+
+<style></style>
