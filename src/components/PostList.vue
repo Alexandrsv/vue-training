39c96c9ac35cbeab39e54976bf556e4ac7ad1280
Александр <zbc@mail.ru>
2022-05-12 16:04:49 +0300

    add v-else logic

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="post_list">
-    <h3>Список записей</h3>
+  <div class="post_list" v-if="posts.length > 0">
+    <h3 style="padding-top: 10px">Список записей</h3>
     <post-item
       v-for="post in posts"
       :post="post"
@@ -8,6 +8,9 @@
       @remove="$emit('remove', post)"
     />
   </div>
+  <h3 v-else style="color: teal; margin-top: 25px">
+    Список записей пуст, их больше нет
+  </h3>
 </template>
 
 <script>
